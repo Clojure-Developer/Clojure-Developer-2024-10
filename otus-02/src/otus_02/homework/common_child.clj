12fde6f296114815edeef2ac1eadf215a4045eb5
Anton Mashkov <slonikmak@gmail.com>
2024-11-12 01:48:15 +0300

    Homework 2

--- otus-02/src/otus_02/homework/common_child.clj
+++ otus-02/src/otus_02/homework/common_child.clj
@@ -15,8 +15,44 @@
 
 ;; Еще пример HARRY и SALLY. Ответ будет - 2, так как общий элемент у них AY
 
+;    A B C D
+;  0 0 0 0 0
+;A 0 1 1 1 1
+;B 0 1 2 2 2
+;D 0 1 2 2 3
+;C 0 1 2 3 3
 
-(defn common-child-length [first-string second-string])
+;  A D
+;A
+;B
 
+(defn common-child-length [s1 s2]
+  (let [memo-lcs (memoize
+                   (fn lcs [i j]
+                     (println (str i " " j))
+                     (println (str (nth s1 (dec i) "-") " " (nth s2 (dec j) "-")))
+                     (cond
+                       (or (zero? i) (zero? j)) 0
+                       (= (nth s1 (dec i)) (nth s2 (dec j)))
+                       (inc (lcs (dec i) (dec j)))
+                       :else
+                       (max (lcs i (dec j)) (lcs (dec i) j)))))]
+    (memo-lcs (count s1) (count s2))))
 
-
+(defn common-child-length-2 [s1 s2]
+  (let [n (count s1)
+        m (count s2)
+        dp (vec (repeat (inc n) (vec (repeat (inc m) 0))))]
+    (loop [i 1 dp dp]
+      (if (> i n)
+        (get-in dp [n m])
+        (recur (inc i)
+               (loop [j 1 dp dp]
+                 (if (> j m)
+                   dp
+                   (recur (inc j)
+                          (assoc-in dp [i j]
+                                    (if (= (nth s1 (dec i)) (nth s2 (dec j)))
+                                      (inc (get-in dp [(dec i) (dec j)]))
+                                      (max (get-in dp [(dec i) j])
+                                           (get-in dp [i (dec j)]))))))))))))
--- otus-02/test/otus_02/homework/fizzbuzz.clj
+++ otus-02/test/otus_02/homework/fizzbuzz.clj
@@ -3,12 +3,19 @@
    [clojure.test :refer :all]))
 
 
+(defn replace-num [n]
+  (cond
+    (and (= (mod n 3) 0) (= (mod n 5) 0))  "FizzBuzz"
+    (= (mod n 3) 0) "Fizz"
+    (= (mod n 5) 0) "Buzz"
+    :else n))
+
 (defn fizz-buzz [n]
   "Создайте программу, которая выводит числа от 1 до n.
    - Если число делится на 3, выведите 'Fizz';
    - если число делится на 5, выведите 'Buzz';
    - если число делится и на 3 и на 5, выведите 'FizzBuzz'."
-  "implement me")
+  (map replace-num (range 1 (+ 1 n))))
 
 
 (deftest fizz-buzz-test
