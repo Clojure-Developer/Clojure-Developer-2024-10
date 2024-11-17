dd8709b4c58d7a9bd26a53f3c936931f3d1c2e14
Gregory Tereshko <gtrsh@users.noreply.github.com>
2024-11-18 01:56:07 +0300

    otus-04 magic-square wip

--- otus-04/src/otus_04/homework/magic_square.clj
+++ otus-04/src/otus_04/homework/magic_square.clj
@@ -1,17 +1,34 @@
 (ns otus-04.homework.magic-square)
 
-;; Оригинальная задача:
-;; https://www.codewars.com/kata/570b69d96731d4cf9c001597
-;;
-;; Подсказка: используйте "Siamese method"
-;; https://en.wikipedia.org/wiki/Siamese_method
+(defn index-overflow-creator [n] (partial < (dec n)))
+(defn next-coord [sq-vec curr-coord] [sq-vec curr-coord])
+
+(defn magic-square-iterate
+  [n m sq-vec coord]
+  (let [square-size n]
+
+    [n m sq-vec coord square-size next-coord]))
 
 (defn magic-square
-  "Функция возвращает вектор векторов целых чисел,
-  описывающий магический квадрат размера n*n,
-  где n - нечётное натуральное число.
-
-  Магический квадрат должен быть заполнен так, что суммы всех вертикалей,
-  горизонталей и диагоналей длиной в n должны быть одинаковы."
   [n]
-  [[0]])
+  {:pre [(odd? n)]}
+
+  (let [magic-vector (vec (repeat n (vector)))
+        initial-index (quot n 2)
+        coordinates {:x 0 :y initial-index}]
+
+    [coordinates magic-vector]))
+
+(comment
+  (magic-square 9)
+  (get [1 2 3 4 5 6 7 8 9] 4)
+  (def n 3)
+  (def index-overflow? (index-overflow-creator n))
+  (index-overflow? 3)
+  (defn iterate-fn [n m]
+    (if (= m 0)
+      n
+      (iterate-fn (* n 2) (dec m))))
+
+  (= (iterate-fn 1 10) 1024)
+)
