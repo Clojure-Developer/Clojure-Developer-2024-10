200109a2002988da647bcc7e8622779911330d8e
Gregory Tereshko <gtrsh@users.noreply.github.com>
2024-11-17 19:49:38 +0300

    complete otus-04/scramblies task

--- otus-04/src/otus_04/homework/scramblies.clj
+++ otus-04/src/otus_04/homework/scramblies.clj
@@ -1,10 +1,40 @@
 (ns otus-04.homework.scramblies)
 
-;; Оригинальная задача:
-;; https://www.codewars.com/kata/55c04b4cc56a697bb0000048
+;; 1-st attempt
+;; первая попытка не учитывала что в letters может быть не достаточно букв для
+;; построения word
+(defn scramble?
+  [letters word]
+  (let [letters-set (set letters)]
+    (every? true?
+            (map (comp not nil? letters-set) word))))
+
+;; 2-nd attempt
+;; вторая попытка учитывает кол-во букв, но результат получаем двумя проходами по
+;; последовательности ключей, решил заменить на один проход при помощи reduce
+(defn reducer [acc val]
+  (if (acc val)
+    (update acc val inc)
+    (assoc acc val 1)))
+
+(defn word->map
+  [word]
+  (reduce reducer {} word))
 
 (defn scramble?
-  "Функция возвращает true, если из букв в строке letters
-  можно составить слово word."
   [letters word]
-  nil)
+  (let [letters-map (word->map letters)
+        word-map (word->map word)
+        word-map-keys (keys word-map)]
+
+    (every? false?
+            (map #(< (letters-map %1 0) (word-map %1 0)) word-map-keys))))
+
+;; 3-rd attempt
+(defn scramble?
+  [letters word]
+  (let [letters-map (word->map letters)
+        word-map (word->map word)
+        word-map-keys (keys word-map)]
+
+    (reduce #(if (< (letters-map %2 0) (word-map %2 0)) (not %1) %1) true word-map-keys)))
