eb8eea9c4e0e5f6ffcade5e879a022329f6f343d
Gregory Tereshko <gtrsh@users.noreply.github.com>
2024-11-21 03:28:03 +0300

    complete otus-04/magic-square task

--- otus-04/src/otus_04/homework/magic_square.clj
+++ otus-04/src/otus_04/homework/magic_square.clj
@@ -1,17 +1,40 @@
 (ns otus-04.homework.magic-square)
 
-(defn get-item
-  [square-vec {:keys [x y]}]
-  (let [row (nth square-vec y)]
+(defn next-index-row-factory
+  [n]
+  (fn [index] (if (= index 0) (dec n) (dec index))))
 
-    (nth row x)))
+(defn next-index-col-factory
+  [n]
+  (fn [index] (if (= index (dec n)) 0 (inc index))))
 
-(defn set-item
-  [square-vec item {:keys [x y]}]
-  (let [row (nth square-vec y)
-        new-row (assoc row x item)]
+(defn next-coords
+  [next-index-col next-index-row {:keys [col row]}]
+  {:row (next-index-row row)
+   :col (next-index-col col)})
 
-    (assoc square-vec y new-row)))
+(defn build-square-map
+  [square-map coords next-coords-fn value square-size]
+  (let [new-value (inc value)
+        next-map (assoc square-map coords new-value)
+        next-coords (next-coords-fn coords)
+        cell-empty? (nil? (get next-map next-coords))
+        next-coords (if cell-empty? next-coords {:row (inc (:row coords)) :col (:col coords)})]
+
+    (if (zero? square-size)
+      next-map (build-square-map next-map next-coords next-coords-fn new-value (dec square-size)))))
+
+(defn build-square-map
+  [square-map coords next-coords-fn value square-size]
+  (if (zero? square-size)
+    square-map
+    (let [new-value (inc value)
+          next-map (assoc square-map coords new-value)
+          next-coords (next-coords-fn coords)
+          cell-empty? (nil? (get next-map next-coords))
+          next-coords (if cell-empty? next-coords {:row (inc (:row coords)) :col (:col coords)})]
+
+      (build-square-map next-map next-coords next-coords-fn new-value (dec square-size)))))
 
 (defn repeat-n-vec
   [item n]
@@ -19,137 +42,27 @@
        (repeat n)
        (vec)))
 
-(defn empty-square-creator
+(defn empty-square-vec
   [n]
   (repeat-n-vec (repeat-n-vec nil n) n))
-
-(defn index-overflow-creator
-  [n]
-  (partial < (dec n)))
-
-(defn inc-coords
-  [{:keys [x y]} index-overflow?]
-
-  {:x (if (index-overflow? (inc x)) 0 (inc x))
-   :y (if (index-overflow? (inc y)) 0 (inc y))})
-
-;; ============================================================
-
-(defn magic-square-iterate [])
 
 (defn magic-square
   [n]
   {:pre [(odd? n)]}
 
-  (let [initial-vector  (empty-square-creator 3) ;;(vec (repeat n (vector)))
-        initial-index (quot n 2)
-        initial-coords {:y 0 :x initial-index}]
+  (let [initial-index (quot n 2)
+        initial-coords {:row 0 :col initial-index}
+        initial-value 0
+        next-index-col (next-index-col-factory n)
+        next-index-row (next-index-row-factory n)
+        next-coords-fn (partial next-coords next-index-col next-index-row)
+        empty-vec (empty-square-vec n)
+        square-map (build-square-map {} initial-coords next-coords-fn initial-value (* n n))]
 
-    (magic-square-iterate)))
+    (reduce (fn [acc {:keys [row col] :as val}]
+              (let [square-val (get square-map val)
+                    square-row (nth acc row)
+                    new-row (assoc square-row col square-val)]
 
-(comment
-  (magic-square 3)
-  (get [1 2 3 4 5 6 7 8 9] 4)
-  (def n 3)
-  (def index-overflow? (index-overflow-creator n))
-  (index-overflow? 3)
-  (defn iterate-fn [n m]
-    (if (= m 0)
-      n
-      (iterate-fn (* n 2) (dec m))))
-
-  (= (iterate-fn 1 10) 1024)
-
-  (vec (repeat n nil))
-
-  ;; params destructuring
-
-  (defn test [a b {:keys [x y]}]
-    [x y])
-
-  (test 1 2 {:x 2 :y 4}))
-
-(comment
-  (def test-vec-3 [[] [8 7 6] []])
-
-  (def x 2)
-  (def y 0)
-  (def item 777)
-
-  (assoc test-vec-3 x
-         (assoc (get test-vec-3 y) y item))
-
-  (def n 5)
-
-  (def empty-square-vec
-    (repeat-n-vec
-     (repeat-n-vec nil n) n))
-  empty-square-vec)
-
-;;      x
-;; [    |
-;;  [1 22 15] ;; <- y
-;;  [6 88 42]
-;;  [0 54 65]
-;; ]
-
-(def n 3)
-
-(def empty-square-vec
-  (repeat-n-vec
-   (repeat-n-vec nil n) n))
-
-empty-square-vec
-
-(def sample-vec
-  (set-item
-   (set-item
-    empty-square-vec
-    77 {:x 1 :y 0})
-   42 {:x 2 :y 0}))
-
-sample-vec
-;; [[nil 77 42] [nil nil nil] [nil nil nil]]
-
-(def index-overflow? (index-overflow-creator n))
-
-(get-item sample-vec {:x 2 :y 0}) ;; 42
-
-;; данная функция не подходит для итеративного процесса напрямую
-;; вместо неё создаем в let диагональные-координаты и шаг-вниз-координаты
-;; проверяем ячейку квадрата на nil и делаем выбор следующих координат
-
-(defn next-coords
-  [square-vec coords index-overflow?]
-  ;; (let [x (coords :x)
-  ;;       y (coords :y)]
-
-  (let [new-diag-coords {}
-        new-down-coords {}]
-
-    (if (nil? (get-item square-vec new-diag-coords))
-      (new-diag-coords) (new-down-coords))))
-
-;; перенеси в let в функции next-coords
-(def diag-coords {:x :y})
-(def step-down-coords {:x :y})
-
-(next-coords sample-vec {:x 1 :y 0} index-overflow?)
-
-(index-overflow? 0) ;; false
-(index-overflow? 2) ;; false
-(index-overflow? 3) ;; true
-
-(def test-a {:x 1 :y 0})
-
-(inc-coords test-a index-overflow?)
-
-;; (update test-a :a (if (index-overflow? (test-a :a)) * inc))
-
-;; Создать hash-map с ключачи парами ячеек квадрата, а далее преобразовать в вектор векторов
-(def test-x
-  {{:x 0 :y 1} 1
-   {:x 1 :y 2} 22})
-
-(get test-x {:x 0 :y 1}) ; 1
-(get test-x {:x 1 :y 4}) ; nil
+                (assoc acc row new-row)))
+            empty-vec (keys square-map))))
