17348d76b385052034404e1bcea847dd81f54487
Artur Taranchiev <github@in.ewok.email>
2024-12-08 22:39:50 +0600

    init-14

--- otus-14/homework/project.clj
+++ otus-14/homework/project.clj
@@ -6,6 +6,7 @@
                  [ring/ring-core "1.10.0"]
                  [ring/ring-jetty-adapter "1.10.0"]
                  [ring/ring-defaults "0.3.4"]
+                 [ring/ring-devel "1.10.0"]
                  [compojure "1.7.0"]
                  [hiccup "1.0.5"]
                  [cheshire "5.11.0"]]
--- otus-14/homework/src/spec_faker/core.clj
+++ otus-14/homework/src/spec_faker/core.clj
@@ -1,13 +1,68 @@
 (ns spec-faker.core
-  (:require [ring.adapter.jetty :refer [run-jetty]]
-            [compojure.core :refer :all]
-            [hiccup.core :refer [html]])
+  (:require
+   [compojure.core :refer [defroutes GET POST]]
+   [compojure.route :as route]
+   [hiccup.core :refer [html]]
+   [hiccup.util :refer [url]]
+   [ring.adapter.jetty :refer [run-jetty]]
+   [ring.util.response :refer [redirect]]
+   [ring.middleware.reload :refer [wrap-reload]]
+   [ring.middleware.params :refer [wrap-params]]
+   [ring.middleware.keyword-params :refer [wrap-keyword-params]]
+   [cheshire.core :refer [generate-string parse-string]])
   (:gen-class))
+
+(defn page [title & body]
+  [:html
+   [:head
+    [:title title]]
+   [:body
+    body]])
+
+(comment
+  (defn set-atom [o t v]
+    (Thread/sleep t)
+    (println (str "set v " v))
+    (+ o v))
+  (def a (atom 0))
+
+  (do
+    (future (swap! a set-atom 5000 100))
+    (Thread/sleep 1000)
+    (future (swap! a set-atom 1000 1))
+   )
+    (Thread/sleep 8000)
+    (println @a))
+
+(defroutes router
+  (GET "/" [spec]
+    (html
+     (page
+      "spec"
+      (if (nil? spec) [:form {:method "POST"}
+                       [:label "Spec: "]
+                       [:textarea {:name "spec" :rows "10" :cols "30"}]
+                       [:button {:type "submit"} "Go..."]]
+          [:h1 (str (parse-string spec))]))))
+
+  (POST "/" [spec]
+    (redirect (str (url "/" {:spec (generate-string spec)}))))
+
+  (route/not-found
+   (html
+    (page
+     "Page not found"
+     [:h1 "Oops!"]))))
+
+(def app
+  (-> #'router
+      wrap-keyword-params
+      wrap-params))
 
 (defn -main
   [& args]
   (run-jetty
-   (routes
-    (GET "/" []
-      (html [:h1 "Hello World!"])))
+
+   (wrap-reload app)
+
    {:port 8000}))
